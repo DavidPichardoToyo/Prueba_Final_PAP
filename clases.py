e27b1aa68c1f0a942fc840d9341c263c97718984
DavidPichardoToyo <dpichardot31@gmail.com>
2025-07-19 03:08:01 +0000

    Avance desafio

--- clases.py
+++ clases.py
@@ -111,25 +111,28 @@ class Campana:
      
     def agregar_anuncios(self):
 
-        
         while True:
             try:
-                opcion = int(input("que tipo de anuncio quieres  1 -  para video - 2 para display - 3 para social"))
+                opcion = int(input("que tipo de anuncio quieres?\n  1. - para video - 2. - para display - 3. - para social"))
                 if opcion == 1:
-                    duracion = int(input("cual es la duracion del video  minimo 5"))
-                    new_anuncio = Video(duracion)
+                    duracion = int(input("cual es la duracion del video (minimo 5): "))
+                    ancho, alto, url_archivo, url_click, sub_tipo = self.peticiones("video")
+                    new_anuncio = Video(duracion, ancho, alto, url_archivo, url_click, sub_tipo)
+                    self.contador_video += 1
                 elif opcion == 2:
-                    new_anuncio = Display()
+                    ancho, alto, url_archivo, url_click, sub_tipo = self.peticiones("display")
+                    new_anuncio = Display(ancho, alto, url_archivo, url_click, sub_tipo)
+                    self.contador_display += 1
                 elif opcion == 3:
-                    new_anuncio = Social()
+                    ancho, alto, url_archivo, url_click, sub_tipo = self.peticiones("social")
+                    new_anuncio = Video(ancho, alto, url_archivo, url_click, sub_tipo)
+                    self.contador_social += 1
                 else:
                     break
-                    
                 self.__anuncios.append(new_anuncio)
-            except Exception as e:
+            except:
                 pass
-        #opcion que el usuario pone la opcion y entramos al bucle while\
-        #self.__anuncios.append(new_anuncio)
+
     @property
     def nombre(self):
         return self.__nombre
@@ -159,24 +162,20 @@ class Campana:
         self.__fecha_termino = fecha_termino
 
     @property
-    def fecha_termino(self):
-        return self.__fecha_termino
-    
-    @property
     def anuncios(self):
         return self.__anuncios
     
-    def __repr__(self):
-        return f"nombre de campana  :{self.nombre} - {self.__anuncios}"
+    def __str__(self):
+        return f"""nombre de campana  :{self.nombre} Anuncios: {self.contador_video} Video, {self.contador_display} Display, {self.contador_social} Social"""
             
 
 class Video(Anuncio):
     FORMATO = "Video"
     SUB_TIPOS = ("INSTREAM" , "OUTSTREAM" )
-
-    def __init__(self,duracion):
-        self.ancho = 1 
-        self.alto = 1
+    def __init__(self, duracion, ancho, alto, url_archivo, url_click, sub_tipo):
+        super().__init__(ancho, alto, url_archivo, url_click, sub_tipo)
+        self.__ancho = 1 
+        self.__alto = 1
         self.__duracion = duracion if duracion > 0 else 5
 
     @property
@@ -187,7 +186,18 @@ class Video(Anuncio):
     def duracion(self,duracion):
         self.__duracion = duracion 
 
-      
+    @property
+    def ancho(self):
+        return self.__ancho
+    
+    @property
+    def alto(self):
+        return self.__alto
+
+    @staticmethod
+    def mostrar_formatos():
+        print(F"{Video.FORMATO}::\n==========\n{Video.SUB_TIPOS[0]}\n{Video.SUB_TIPOS[1]}")
+
     def comprimir_anuncio():
         print("COMPRESIÓN DE VIDEO NO IMPLEMENTADA AÚN")
     
@@ -205,11 +215,17 @@ class Display(Anuncio):
     def __init__(self, ancho, alto, url_archivo, url_click, sub_tipo):
         super().__init__(ancho, alto, url_archivo, url_click, sub_tipo)
 
+    @staticmethod
+    def mostrar_formatos():
+        print(F"{Display.FORMATO}::\n==========\n{Display.SUB_TIPOS[0]}\n{Display.SUB_TIPOS[1]}")
+        
+
     def comprimir_anuncio():
-        print("COMPRESIÓN DE VIDEO NO IMPLEMENTADA AÚN")
-    
-    def redimensionar_anuncio():
-        print( "RECORTE DE VIDEO NO IMPLEMENTADO AÚN")
+        print("COMPRESIÓN DE ANUNCIOS DISPLAY NO IMPLEMENTADA AÚN")
+
+
+    def redimencionar_anuncio():
+        print("REDIMENSIONAMIENTO DE ANUNCIOS DISPLAY NO IMPLEMENTADO AÚN")
 
     def __repr__(self):
         return f"{Display.FORMATO}"
@@ -221,11 +237,15 @@ class Social(Anuncio):
     def __init__(self, ancho, alto, url_archivo, url_click, sub_tipo):
         super().__init__(ancho, alto, url_archivo, url_click, sub_tipo)
 
+    @staticmethod
+    def mostrar_formatos():
+        print(F"{Social.FORMATO}:\n==========\n{Social.SUB_TIPOS[0]}\n{Social.SUB_TIPOS[1]}")
+
     def comprimir_anuncio():
-        print("COMPRESIÓN DE VIDEO NO IMPLEMENTADA AÚN")
-    
-    def redimensionar_anuncio():
-        print( "RECORTE DE VIDEO NO IMPLEMENTADO AÚN")
-    
+        print("COMPRESIÓN DE ANUNCIOS DE REDES SOCIALES NO IMPLEMENTADA AÚN")
+
+    def redimencionar_anuncio():
+        print("REDIMENSIONAMIENTO DE ANUNCIOS DE REDES SOCIALES NO IMPLEMENTADO AÚN")
+
     def __repr__(self):
         return f"{Social.FORMATO}"
