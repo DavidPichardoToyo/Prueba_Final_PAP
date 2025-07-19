ec9d4ed6804bada5560696dc742561c1adf887c2
DavidPichardoToyo <dpichardot31@gmail.com>
2025-07-19 03:30:16 +0000

    desafío

--- main.py
+++ main.py
@@ -1,4 +1,25 @@
 from clases import *
+import datetime
+import pytz
 
+tz_CL = pytz.timezone("America/Santiago")
+datetime_CL = datetime.datetime.now(tz_CL)
 
-a = Video(5)
+un_mes = datetime_CL + datetime.timedelta(days=30)
+hoy = datetime_CL.strftime("%d/%m/%Y")
+
+cam = Campana("Prueba", hoy, un_mes.strftime("%d/%m/%Y"))
+
+try:
+    nuevo_nombre_campana = input("Ingrese el nuevo nombre de la campaña: ")
+    cam.nombre = nuevo_nombre_campana
+
+    print("Los subtipos soportados para el siguiente formato son:")
+    Video.mostrar_formatos()
+    nuevo_sub_tipo = input("\nIngrese el nuevo subtipo: ")
+
+    cam.anuncios[0].sub_tipo = nuevo_sub_tipo
+except Exception as e:
+    with open("error.log", "a+") as log:
+        log.write(f"{datetime_CL.strftime('%d/%m/%Y %H:%M:%S')}, {e} \n")
+
